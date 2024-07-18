cb063e3920137bfc0382679af61895cd0fdcd34f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-07-18 11:02:25 +0200

    Replace Repository by ListCrudRepository

--- src/main/java/fr/sncf/d2d/colitrack/domain/AppUserRepository.java
+++ src/main/java/fr/sncf/d2d/colitrack/domain/AppUserRepository.java
@@ -1,14 +1,6 @@
 package fr.sncf.d2d.colitrack.domain;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.ListCrudRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface AppUserRepository extends Repository<AppUser, String> {
-
-    AppUser save(AppUser input);
-    Optional<AppUser> findById(String id);
-    List<AppUser> findAll();
-    void deleteById(String id);
+public interface AppUserRepository extends ListCrudRepository<AppUser, String> {
 }

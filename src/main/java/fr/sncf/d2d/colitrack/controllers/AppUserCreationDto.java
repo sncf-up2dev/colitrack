9dd41608ffe3fac5c644f3a944ef2262b1bc5c61
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-07-25 12:10:55 +0200

    Cross field validation

--- src/main/java/fr/sncf/d2d/colitrack/controllers/AppUserCreationDto.java
+++ src/main/java/fr/sncf/d2d/colitrack/controllers/AppUserCreationDto.java
@@ -1,6 +1,7 @@
 package fr.sncf.d2d.colitrack.controllers;
 
 import fr.sncf.d2d.colitrack.domain.AppUser;
+import jakarta.validation.constraints.AssertTrue;
 
 public record AppUserCreationDto(
         @Username
@@ -8,6 +9,11 @@ public record AppUserCreationDto(
         @Password
         String password
 ) {
+
+    @AssertTrue(message = "Password should not contain username")
+    public boolean isUsernameInPassword() {
+        return !this.password.contains(this.username);
+    }
 
     public AppUser toAppUser() {
         return new AppUser(username, password);

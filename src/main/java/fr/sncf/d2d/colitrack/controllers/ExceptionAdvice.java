3b5af3011354f3465b3cd75f8149c424963178a3
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-07-25 11:06:14 +0200

    Personalize validation message

--- src/main/java/fr/sncf/d2d/colitrack/controllers/ExceptionAdvice.java
+++ src/main/java/fr/sncf/d2d/colitrack/controllers/ExceptionAdvice.java
@@ -3,9 +3,13 @@ package fr.sncf.d2d.colitrack.controllers;
 import fr.sncf.d2d.colitrack.domain.DuplicateException;
 import fr.sncf.d2d.colitrack.domain.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
@@ -20,5 +24,22 @@ public class ExceptionAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorDto handle(DuplicateException exception) {
         return ErrorDto.from(exception);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handle(MethodArgumentNotValidException ex) {
+        if (ex.getBindingResult().getAllErrors().isEmpty()) {
+            return ErrorDto.from(ex);
+        }
+        String errors = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> {
+                    String name = (error instanceof FieldError fieldError) ?
+                            fieldError.getField() :
+                            error.getObjectName();
+                    return "In [%s]: %s".formatted(name, error.getDefaultMessage());
+                })
+                .collect(Collectors.joining(", "));
+        return new ErrorDto(errors);
     }
 }

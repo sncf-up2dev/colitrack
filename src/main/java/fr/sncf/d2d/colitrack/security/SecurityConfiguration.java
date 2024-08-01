e0faaa40babf3115cf2a5b54a1d416eeb9698a24
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-08-01 10:47:45 +0200

    Enable security

---
+++ src/main/java/fr/sncf/d2d/colitrack/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package fr.sncf.d2d.colitrack.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
